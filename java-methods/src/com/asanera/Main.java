d7b6cceea51a24c1871fc5df0de56e64ceb49a8a
Alvaro <alvaro@asanera.com>
2022-05-04 21:55:32 +0200

    [Refactor] - Rename var result

--- java-methods/src/com/asanera/Main.java
+++ java-methods/src/com/asanera/Main.java
@@ -8,29 +8,32 @@ public class Main {
 	    Ejercice ejercice = new Ejercice();
 
         //Write a Java method to find the smallest number among three numbers
-         int resutl = ejercice.findSmallNumber(25,37,29);
-         System.out.println(resutl);
+         int result = ejercice.findSmallNumber(25,37,29);
+         System.out.println(result);
 
         //Write a Java method to find the smallest number among three numbers
-         resutl = ejercice.findEvergeNumber(25,45,65);
-         System.out.println(resutl);
+         result = ejercice.findEvergeNumber(25,45,65);
+         System.out.println(result);
 
          // Write a Java method to count all vowels in a string.
-        resutl = ejercice.countVowels("e");
-        System.out.println(resutl);
+        result = ejercice.countVowels("e");
+        System.out.println(result);
 
         //Write a Java method to count all words in a string.
-        resutl = ejercice.countWord("The quick brown fox jumps over the lazy dog.");
-        System.out.println(resutl);
+        result = ejercice.countWord("The quick brown fox jumps over the lazy dog.");
+        System.out.println(result);
 
         //Write a Java method to compute the sum of the digits in an integer
-        resutl = ejercice.sumDigit(25);
-        System.out.println(resutl);
+        result = ejercice.sumDigit(25);
+        System.out.println(result);
 
         //Write a Java method to display the first 50 pentagonal numbers
         for (int num = 1; num <=50; num++){
             System.out.print(ejercice.calculatePentagonalNumber(num) + (num % 10 == 0 ? "\n" : " "));
         }
+
+        //Write a Java method to compute the future investment value at a given interest rate for a specified number of years.
+
 
 
     }
